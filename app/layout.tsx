3d42040c5895c9b9db25dc5615b52e392fbf84d9
mac <mac@MacBook-Pro-2.local>
2026-01-05 03:12:53 -0300

    update meditation copy and routines

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,8 @@ export default function RootLayout({
         {/* PWA */}
         <link rel="manifest" href="/manifest.json" />
 
+        <meta name="mobile-web-app-capable" content="yes" />
+
         {/* iOS PWA */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
