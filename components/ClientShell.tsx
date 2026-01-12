9109181e9617043ee29793c3dbeeb9478b45d3b6
mac <mac@MacBook-Pro-2.local>
2026-01-12 02:48:09 -0300

    Add protected routes with Supabase auth layout

--- components/ClientShell.tsx
+++ components/ClientShell.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 export default function ClientShell({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       {children}
     </>
   );
